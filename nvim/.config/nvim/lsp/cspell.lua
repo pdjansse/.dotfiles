1352cc5b9ffbf5ce78ab738000e5c524a1050be9
Patrick Janssen <patrick.daniel.janssen@gmail.com>
2025-06-01 18:25:31 -0400

    nvim add cspell lsp

---
+++ nvim/.config/nvim/lsp/cspell.lua
@@ -0,0 +1,18 @@
+return {
+    cmd = { "cspell-lsp", "--stdio" },
+    root_markers = {
+        ".git",
+        "cspell.json",
+        ".cspell.json",
+        "cspell.json",
+        ".cSpell.json",
+        "cSpell.json",
+        "cspell.config.js",
+        "cspell.config.cjs",
+        "cspell.config.json",
+        "cspell.config.yaml",
+        "cspell.config.yml",
+        "cspell.yaml",
+        "cspell.yml",
+    },
+}
--- nvim/.config/nvim/lua/nconf/remap.lua
+++ nvim/.config/nvim/lua/nconf/remap.lua
@@ -1,6 +1,12 @@
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set(
+    "n",
+    "<leader>ca",
+    vim.lsp.buf.code_action,
+    { noremap = true, silent = true, desc = "LSP Code Action" }
+)
 
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlights text when yanking",
