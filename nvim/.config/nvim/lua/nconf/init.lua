2f24e7542e7aab777982bc99f8edd8c396fae4bb
Patrick Janssen <patrick.daniel.janssen@gmail.com>
2025-06-01 16:14:30 -0400

    nvim fix neovim vscode

--- nvim/.config/nvim/lua/nconf/init.lua
+++ nvim/.config/nvim/lua/nconf/init.lua
@@ -1,6 +1,9 @@
-vim.g.mapleader = " "
-
-require("nconf.lazy_init")
-require("nconf.set")
-require("nconf.remap")
-require("nconf.lsp")
+if vim.g.vscode then
+    -- pass
+else
+    vim.g.mapleader = " "
+    require("nconf.lazy_init")
+    require("nconf.set")
+    require("nconf.remap")
+    require("nconf.lsp")
+end
